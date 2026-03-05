e6982041fe83e59504771857333f69f3a86fccc3
Mohammed Zain <87908878+maximilum@users.noreply.github.com>
2026-03-05 16:41:48 +0300

    dseign the hero section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,33 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ArrowBigRight, ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className=" flex flex-col bg-background min-h-screen">
+      <main className="flex-1">
+        {/* Hero section */}
+        <section className="container mx-auto px-4 py-32">
+          <div className="max-w-4xl mx-auto text-center ">
+            <h1 className="font-extrabold text-6xl mb-6">
+              Never Lose <span className="text-muted-foreground">Track</span> of
+              an Opportunity Again
+            </h1>
+            <p className="text-xl text-muted-foreground mb-10">
+              From first application to final offer, manage your entire job
+              search journey in one beautiful dashboard. Set reminders, track
+              progress, and never let another opportunity slip through the
+              cracks.
+            </p>
+            <div className="flex flex-col items-center gap-4">
+              <Button size={"xl"} className=" h-16 px-16 font-semibold text-md">
+                Start Free <ArrowBigRight className="ml-2 size-6" />
+              </Button>
+              <p className="text-sm text-muted-foreground">
+                it will always be free, No credit card. No trials. No catch.
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
