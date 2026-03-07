f1c0dce8c701898ad756799c40ff891aa4667854
Mohammed Zain <87908878+maximilum@users.noreply.github.com>
2026-03-07 14:19:03 +0300

    configure mongoDB and mongoose

---
+++ lib/db.tsx
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+interface mongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: mongooseCache | undefined;
+}
+
+const cached: mongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
+const MONGO_URI = process.env.MONGODB_URI;
+
+async function connectDB() {
+  if (!MONGO_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env",
+    );
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(MONGO_URI, opts);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    console.log("database connected");
+  } catch (error) {
+    console.log("error connectiong to database", error);
+    cached.promise = null;
+  }
+  return cached.conn;
+}
+
+export default connectDB;
