ad40e34fa0249fccc4c4eaf5ec08f3ffdf78e095
Mohammed Zain <87908878+maximilum@users.noreply.github.com>
2026-03-07 16:09:27 +0300

    create sign up page

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,7 +1,87 @@
-import React from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    setIsLoading(true);
+    console.log(name, email, password);
+  }
+
+  return (
+    <div className="min-h-[calc(100vh-52px)] w-full flex justify-center items-center">
+      <Card className="p-4 w-96">
+        <CardTitle>
+          <h1>Sign Up</h1>
+        </CardTitle>
+        <form onSubmit={handleSubmit}>
+          <CardContent className="flex flex-col gap-4 mb-8">
+            <div>
+              <Label htmlFor="name" className="mb-1">
+                Name
+              </Label>
+              <Input
+                id="name"
+                type="text"
+                required
+                placeholder="John Doe"
+                className="placeholder:text-neutral-300"
+              ></Input>
+            </div>
+            <div>
+              <Label htmlFor="email" className="mb-1">
+                Email
+              </Label>
+              <Input
+                id="email"
+                type="email"
+                required
+                placeholder="John@doe.com"
+                className="placeholder:text-neutral-300"
+              ></Input>
+            </div>
+            <div>
+              <Label htmlFor="password" className="mb-1">
+                Password
+              </Label>
+              <Input id="password" type="password" required></Input>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col gap-4">
+            <Button type="submit" className="w-full" disabled={isLoading}>
+              {isLoading ? "Signing up..." : "Sign up"}
+            </Button>
+            {error && (
+              <p className="text-center  text-xs text-destructive">{error}</p>
+            )}
+            <p className="text-xs">
+              if you have an account{"  "}
+              <Link
+                href="/sign-in"
+                className="text-primary font-semibold hover:text-primary/50 transition-colors ml-2"
+              >
+                sign in
+              </Link>
+            </p>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default SignUp;
