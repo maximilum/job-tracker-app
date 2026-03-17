473980ba6ca2bc3d1e612f73b774fe01ae7dccd2
Mohammed Zain <87908878+maximilum@users.noreply.github.com>
2026-03-18 02:33:07 +0300

    create a profile and dashboard link in navbar
    create a profile and dashboard link in navbar when signed in. create server actions for sign out and getSessions

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,46 @@
-import { IdCardLanyard } from "lucide-react";
+"use client";
+
+import { IdCardLanyard, LogOut } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import { useSession, signOut } from "@/lib/auth/auth-client";
+import { Redirect } from "next";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Avatar,
+  AvatarBadge,
+  AvatarFallback,
+  AvatarGroup,
+  AvatarGroupCount,
+  AvatarImage,
+} from "@/components/ui/avatar";
+import { redirect } from "next/navigation";
 
 const Navbar = () => {
+  const session = useSession();
+  const user = session.data?.user;
+
+  const handleSignOut = async () => {
+    const result = await signOut();
+    if (result.data?.success) {
+      redirect("/sign-in");
+    }
+  };
+
   return (
     <nav className="border-b border-border bg-background text-foreground">
       <div className="container flex mx-auto px-4 h-16 items-center justify-between">
@@ -13,13 +50,60 @@ const Navbar = () => {
             <p>Job Tracker</p>
           </div>
         </Link>
-        <div className="flex gap-2">
-          <Link href={"/sign-in"}>
-            <Button variant={"ghost"}>Sign In</Button>
-          </Link>
-          <Link href={"/sign-up"}>
-            <Button>Sign Up</Button>
-          </Link>
+        <div className="flex items-center gap-4">
+          {!user ? (
+            <div className="flex gap-2">
+              <Link href={"/sign-in"}>
+                <Button variant={"ghost"}>Sign In</Button>
+              </Link>
+              <Link href={"/sign-up"}>
+                <Button>Sign Up</Button>
+              </Link>
+            </div>
+          ) : (
+            <>
+              <DropdownMenu modal={false}>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="relative h-8 w-8 mr-4 rounded-full"
+                  >
+                    <Avatar>
+                      <AvatarImage src="" alt="@shadcn" className="grayscale" />
+                      <AvatarFallback>
+                        {user.name[0].toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  className="w-70 absolute right-0"
+                  align="end"
+                >
+                  <DropdownMenuGroup>
+                    <DropdownMenuLabel>{user.name}</DropdownMenuLabel>
+                    <DropdownMenuLabel>
+                      <p className="text-xs text-gray-500">{user.email}</p>
+                    </DropdownMenuLabel>
+                  </DropdownMenuGroup>
+                  <DropdownMenuSeparator />
+                  <button onClick={handleSignOut} className="w-full">
+                    <DropdownMenuGroup>
+                      <DropdownMenuItem>
+                        Log out
+                        <DropdownMenuShortcut>
+                          <LogOut />
+                        </DropdownMenuShortcut>
+                      </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                  </button>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <Link href={"/dashboard"}>
+                <Button variant={"ghost"}>Dashboard</Button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -2,6 +2,8 @@ import { DBAdapter } from "./../../node_modules/@better-auth/core/src/db/adapter
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import connectDB from "../db";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const mongooseInstance = await connectDB();
 const client = mongooseInstance.connection.getClient();
@@ -15,3 +17,21 @@ export const auth = betterAuth({
     enabled: true,
   },
 });
+
+export async function getSession() {
+  const headersList = await headers();
+  return await auth.api.getSession({
+    headers: headersList,
+  });
+}
+
+export async function signOut() {
+  const headersList = await headers();
+  const result = await auth.api.signOut({
+    headers: headersList,
+  });
+
+  if (result?.success) {
+    redirect("/sign-in");
+  }
+}
